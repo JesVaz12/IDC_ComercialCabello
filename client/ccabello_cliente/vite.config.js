a4ebdc8195d80aae3436649ebb1dbd11a907a284
braulio <carreonriojas@gmail.com>
2025-10-09 16:49:12 -0600

    Corrige sintaxis en vite.config.js

--- client/ccabello_cliente/vite.config.js
+++ client/ccabello_cliente/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom' // <-- ESTA LÍNEA ES LA SOLUCIÓN
-    setupFiles: './src/setupTests.js', // <-- AÑADE ESTA LÍNEA
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js'
   },
 })
