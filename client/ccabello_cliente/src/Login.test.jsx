be752469ae81292db929f6a53fdbbab45215b67f
braulio <carreonriojas@gmail.com>
2025-10-09 16:03:11 -0600

    Añade etapas de prueba y compilación al pipeline de CI

---
+++ client/ccabello_cliente/src/Login.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
+import Login from './Login';
+
+describe('Login Component', () => {
+  it('debería renderizar el campo de usuario', () => {
+    // Renderizamos el componente "en memoria" para la prueba
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
+
+    // Buscamos si el texto "Usuario" existe en el componente
+    const userInput = screen.getByText(/Usuario/i);
+
+    // Esperamos que el elemento exista
+    expect(userInput).toBeInTheDocument();
+  });
+});
