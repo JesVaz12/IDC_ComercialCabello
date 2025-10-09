d6eef3025a87bb91e7238776d7e8ee0d132123df
braulio <carreonriojas@gmail.com>
2025-10-09 16:40:18 -0600

    Añade jest-dom y configura setupFiles para las pruebas

--- client/ccabello_cliente/vite.config.js
+++ client/ccabello_cliente/vite.config.js
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom' // <-- ESTA LÍNEA ES LA SOLUCIÓN
+    setupFiles: './src/setupTests.js', // <-- AÑADE ESTA LÍNEA
   },
 })
